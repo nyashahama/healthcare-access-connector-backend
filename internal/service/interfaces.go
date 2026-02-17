690e55e99bc4f1e5936faa7b2801ca6cd3749b9c
Nyasha Hama <nyashahama45@gmail.com>
2026-02-17 23:04:48 +0200

    added GetStaffByUserID and GetAllClinicsStaff methods to the StaffService interface

--- internal/service/interfaces.go
+++ internal/service/interfaces.go
@@ -273,6 +273,8 @@ type ClinicService interface {
 type StaffService interface {
 	CreateStaffMember(ctx context.Context, staff providers.ClinicStaff) (providers.ClinicStaff, error)
 	GetStaffByID(ctx context.Context, id uuid.UUID) (providers.ClinicStaff, error)
+	GetStaffByUserID(ctx context.Context, userID uuid.UUID) (providers.ClinicStaff, error)
+	GetAllClinicStaff(ctx context.Context, clinicID uuid.UUID) ([]providers.ClinicStaff, error)
 	UpdateStaffMember(ctx context.Context, staff providers.ClinicStaff) error
 	DeleteStaffMember(ctx context.Context, id uuid.UUID) error
 
